e627fde137c0a4d870a7b14c43d5bb754dcf39c9
LisaWan33 <lisayes396@gmail.com>
2023-02-12 16:49:36 +0800

    ⌜ 商品功能 - 實作新增商品 ⌟

--- src/main/java/com/lisawan/springbootmall/dao/ProductDao.java
+++ src/main/java/com/lisawan/springbootmall/dao/ProductDao.java
@@ -1,7 +1,10 @@
 package com.lisawan.springbootmall.dao;
 
+import com.lisawan.springbootmall.dto.ProductRequest;
 import com.lisawan.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/lisawan/springbootmall/service/ProductService.java
+++ src/main/java/com/lisawan/springbootmall/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.lisawan.springbootmall.service;
 
+import com.lisawan.springbootmall.dto.ProductRequest;
 import com.lisawan.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
